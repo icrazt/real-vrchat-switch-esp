ec9ef648b7bc149556b4fc17e7e832298f116843
crazt <i@crazt.moe>
2025-09-27 02:38:54 +0800

    Blink test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
 #include <Arduino.h>
 
-// 按键引脚
+// 按键定义
 #define KEY_USER_2     2
-#define KEY_USER_MAIN  4     //主按键, 当前版本外壳唯一的操作按键
-#define KEY_USER_BOOT  9     //BOOT模式切换按键，正常启动后也可作为用户按键
-// LED引脚
-#define PIN_LED        8     //白色指示灯引脚
+#define KEY_USER_MAIN  4     // 主按键, 当前版本外壳唯一的操作按键
+#define KEY_USER_BOOT  9     // BOOT模式切换按键，正常启动后也可作为用户按键
+// LED定义
+#define PIN_LED        8     // 白色指示灯引脚
 #define PIN_RGB        3     // WS2812 灯珠数据引脚
 #define PIN_RGB_EN     10    // WS2812 灯珠供电使能
 #define RGB_NUM        22    // WS2812 灯珠数量
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr unsigned long kBlinkIntervalMs = 500;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(PIN_LED, HIGH);
+  delay(kBlinkIntervalMs);
+  digitalWrite(PIN_LED, LOW);
+  delay(kBlinkIntervalMs);
 }
